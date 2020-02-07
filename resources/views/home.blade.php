59d8c5ab635a060afee17bc09a8e3e47ee35dfa5
vdediego <vdediego@atsistemas.com>
2020-02-07 13:16:07 +0100

    add username to authentication

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,55 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://i.ebayimg.com/images/g/JpcAAOSw2gxYzPkj/s-l1600.jpg" class="rounded-circle logo-img">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>Title</h1>
             </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>154</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> views</div>
+                <div class="pr-5"><strong>212</strong> followers</div>
+            </div>
+            <div class="pt-4 font-weight-bold">Lorem ipsum</div>
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet animi dicta dignissimos, dolor eos ex explicabo illo incidunt ipsam iure laboriosam magnam maiores molestiae necessitatibus nesciunt perferendis, tempora, veritatis!</div>
+            <div><a href="#">www.google.de</a></div>
         </div>
     </div>
-</div>
+    <div class="row pt-4">
+        <div class="col-4 pr-3">
+            <img class="w-100" src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.179.1440.1440a/s640x640/83366394_177251956986760_4778819387883714164_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=0FBVoP-NJdAAX_Fmkv2&oh=59bc9da5099f95ce7c816abe70b01bb8&oe=5ECA50D1" alt="">
+        </div>
+        <div class="col-4 pr-3">
+            <img class="w-100" src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.179.1440.1440a/s640x640/83366394_177251956986760_4778819387883714164_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=0FBVoP-NJdAAX_Fmkv2&oh=59bc9da5099f95ce7c816abe70b01bb8&oe=5ECA50D1" alt="">
+        </div>
+        <div class="col-4 pr-3">
+            <img class="w-100" src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.179.1440.1440a/s640x640/83366394_177251956986760_4778819387883714164_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=0FBVoP-NJdAAX_Fmkv2&oh=59bc9da5099f95ce7c816abe70b01bb8&oe=5ECA50D1" alt="">
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-4 pr-3">
+            <img class="w-100" src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.90.720.720a/s640x640/81827123_166842011236779_3394507145247926188_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=xNGemP0siF8AX9sc28w&oh=11babaca2a88ee5125f0c19498c5622e&oe=5E3FBBC7" alt="">
+        </div>
+        <div class="col-4 pr-3">
+            <img class="w-100" src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.90.720.720a/s640x640/81827123_166842011236779_3394507145247926188_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=xNGemP0siF8AX9sc28w&oh=11babaca2a88ee5125f0c19498c5622e&oe=5E3FBBC7" alt="">
+        </div>
+        <div class="col-4 pr-3">
+            <img class="w-100" src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.90.720.720a/s640x640/81827123_166842011236779_3394507145247926188_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=xNGemP0siF8AX9sc28w&oh=11babaca2a88ee5125f0c19498c5622e&oe=5E3FBBC7" alt="">
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-4 pr-3">
+            <img class="w-100" src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.160.1279.1279a/s640x640/79951566_2758472754217731_1788201732702317284_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=UrzjsgjMKg4AX9PeDUR&oh=845b3a1402b829608f734636974badd4&oe=5EBC4037" alt="">
+        </div>
+        <div class="col-4 pr-3">
+            <img class="w-100" src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.160.1279.1279a/s640x640/79951566_2758472754217731_1788201732702317284_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=UrzjsgjMKg4AX9PeDUR&oh=845b3a1402b829608f734636974badd4&oe=5EBC4037" alt="">
+        </div>
+        <div class="col-4 pr-3">
+            <img class="w-100" src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.160.1279.1279a/s640x640/79951566_2758472754217731_1788201732702317284_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=UrzjsgjMKg4AX9PeDUR&oh=845b3a1402b829608f734636974badd4&oe=5EBC4037" alt="">
+        </div>
+    </div>
 @endsection
